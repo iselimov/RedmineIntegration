0f87dcc0187c7081f87565e84466db54eafc321c
defrag <iselimov92@gmail.com>
2017-08-29 08:57:14 +0700

    Добавил формы лоигрования времени и для тасок

---
+++ src/com/defrag/redmineplugin/view/ValidatedFormWrapper.java
@@ -0,0 +1,44 @@
+package com.defrag.redmineplugin.view;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.ValidationInfo;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Created by defrag on 29.08.17.
+ */
+public abstract class ValidatedFormWrapper extends DialogWrapper {
+
+    private final ValidatedDialog validatedDialog;
+
+    public ValidatedFormWrapper(@Nullable Project project, ValidatedDialog validatedDialog) {
+        super(project);
+        this.validatedDialog = validatedDialog;
+
+        init();
+        setTitle(getTitleName());
+        setValidationDelay(1000);
+    }
+
+    @Nullable
+    @Override
+    protected ValidationInfo doValidate() {
+        return validatedDialog.getValidationInfo().orElse(null);
+    }
+
+    @Override
+    protected void doOKAction() {
+        super.doOKAction();
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        return validatedDialog.getContentPane();
+    }
+
+    protected abstract String getTitleName();
+}
